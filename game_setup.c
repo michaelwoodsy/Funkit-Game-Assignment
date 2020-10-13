c77b5a131763e9331fe9a2d1a72aa50c9e4ff4e2
Michael Woodard <mdw85@uclive.ac.nz>
2020-10-13 21:46:33 +1300

    Completed file and added documentation

--- game_setup.c
+++ game_setup.c
@@ -1,20 +1,22 @@
-/** @file   game.c
+/** @file   game_setup.c
     @author Michael Woodard and Sean Lalor
-    @date   7 Oct 2020
-    @note   does nothing
+    @date   10 Oct 2020
+    @brief  This is used to initialise and start the game.
 */
+
 
 #include "system.h"
 #include "tinygl.h"
 #include "navswitch.h"
 #include "pacer.h"
 #include "ir_uart.h"
-#include "pio.h"
-#include "../fonts/font3x5_1.h"
 #include "text_output.h"
 
-#define PACER_RATE 500
+#define PACER_RATE 500 /* Sets the pacer. */
 
+/* Initialises the system, infra-red receiver and the pacer.
+   It also calls the function to present the welcoming text
+   to the player. */
 void init_game(void) {
 	system_init();
 	ir_uart_init();
@@ -22,6 +24,8 @@ void init_game(void) {
 	pacer_init(PACER_RATE);
 }
 
+/* Waits for the player to press down on the navswitch. Once
+   the navswitch is pressed, the game starts. */
 void start_game(void) {
 	while (1) {
 		pacer_wait();
