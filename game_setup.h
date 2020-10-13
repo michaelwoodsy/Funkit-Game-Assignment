5a3c73cef988ca6d35134e0453219c9ab751fb4c
Michael Woodard <mdw85@uclive.ac.nz>
2020-10-13 21:46:38 +1300

    Completed file and added documentation

--- game_setup.h
+++ game_setup.h
@@ -1,8 +1,9 @@
-/** @file   game.c
+/** @file   game_setup.h
     @author Michael Woodard and Sean Lalor
-    @date   7 Oct 2020
-    @note   does nothing
+    @date   10 Oct 2020
+    @brief  This is the interface used to initialise and start the game.
 */
+
 #ifndef GAME_SETUP_H
 #define GAME_SETUP_H
 
@@ -11,13 +12,17 @@
 #include "navswitch.h"
 #include "pacer.h"
 #include "ir_uart.h"
-#include "pio.h"
-#include "../fonts/font3x5_1.h"
+#include "text_output.h"
 
-#define PACER_RATE 500
+#define PACER_RATE 500 /* Sets the pacer. */
 
+/* Initialises the system, infra-red receiver and the pacer.
+   It also calls the function to present the welcoming text
+   to the player. */
 void init_game(void);
 
+/* Waits for the player to press down on the navswitch. Once
+   the navswitch is pressed, the game starts. */
 void start_game(void);
 
 #endif
